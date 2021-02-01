2112a047d90c1b72294269d0c9d61ddc4d411774
Vladimir Pravotorov <devpravotorov@gmail.com>
2021-02-01 17:32:14 +0300

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+        <Header />
+    </>
   );
 }
 
